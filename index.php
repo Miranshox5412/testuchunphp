b67aeb6735150bc92dade1dffef4e65ebeea66db
Miranshox5412 <m62708630@gmail.com>
2026-05-06 19:17:57 +0500

    Bu ikkinchi commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>O'quvchilar bahosi</title>
+</head>
+<body>
+
+<?php
+echo "<table border='1'>
+<tr>
+<th>Ism</th>
+<th>Baho</th>
+</tr>
+
+<tr>
+<td>Ali</td>
+<td>5</td>
+</tr>
+
+<tr>
+<td>Vali</td>
+<td>4</td>
+</tr>
+
+</tr>
+<td>Sami</td>
+<td>3</td>
+</tr>
+
+<tr>
+<td>Lola</td>
+<td>56</td>
+</tr>
+
+<tr>
+<td>Aziza</td>
+<td>4</td>
+</tr>
+
+</table>";
+?>
+
+</body>
+</html>
